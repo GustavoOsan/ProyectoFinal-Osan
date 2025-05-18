b6c405590b472eeb091afefefe734c9e5dcfe679
Gustavo Osan <gustavoosan@gmail.com>
2025-05-18 16:01:46 -0300

    Navbar actualizado2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,22 +1,32 @@
 import { use, useEffect, useState } from 'react'
 import { getProducts } from '../mock/asyncMock'
 import ItemList from './ItemList'
+import { useParams } from 'react-router-dom'
+import { SiPanasonic } from 'react-icons/si'
 
 const ItemListContainer = ({ greeting }) => {
 
 
     const [products, setProducts] = useState([])
+    const {categoryId} = useParams()
+
 
     useEffect(() => {
         getProducts()
-            .then((res) => setProducts(res))
+            .then((res) => {
+                if (categoryId) {
+                    setProducts(res.filter((prod) => prod.category === categoryId))
+                } else {
+                    setProducts(res)
+                }
+            })
             .catch((error) => { console.error(error) })
-    }, [])
+    }, [categoryId])
     console.log(products)
 
     return (
         <div>
-            <h1>{greeting}</h1>
+            <h1 className="text-success" >{greeting}{categoryId&& <span>{categoryId}</span>}</h1>
             <ItemList products={products} />
         </div>
     )
